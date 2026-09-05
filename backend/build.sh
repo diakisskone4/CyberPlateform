7aa527ba105a9d0e3119007b182d2945ee60217a
Diakaria Kone <diakisskone45@gmail.com>
2026-09-05 10:55:57 +0000

    buildNew

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
 set -o errexit
-pip install -r requirements.txt 
+
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crée le superuser seulement s'il n'existe pas déjà (ne fait pas planter le build sinon)
+python manage.py createsuperuser --noinput || echo "Superuser déjà existant ou variables non définies, on continue."
